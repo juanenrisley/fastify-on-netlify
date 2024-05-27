2323aa61aebf6b00843639a7cc242fa2f156ecd5
Juan Enrique Alcaraz <juanenrisley@gmail.com>
2024-05-27 16:28:16 +0200

    Set build with parcel

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ function init() {
   return app;
 }
 
-if (require.main === module) {
-  // called directly i.e. "node app"
-  init().listen({ port: 3000 }, (err) => {
-    if (err) console.error(err);
-    console.log('server listening on 3000');
-  });
-} else {
-  // required as a module => executed on aws lambda
-  module.exports = init;
-}
+init().listen({ port: 3000 }, (err) => {
+  if (err) console.error(err);
+  console.log('server listening on 3000');
+});
